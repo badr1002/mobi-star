c7344628f40d8a2aa2afc238cf8152d1776c284f
badr1002 <baderhelal567@gmail.com>
2021-09-05 10:34:59 +0200

    s11

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { OrderService } from './service/order.service';
 import { UserService } from './service/user.service';
 import { Router } from '@angular/router';
-import { Component, Input } from '@angular/core';
+import { Component, Input,AfterViewInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent{
+export class AppComponent implements AfterViewInit {
   title = 'Mobi-star';
   arrowStatus: any = true;
+  screenLoading: any = true;
   constructor(public _user: UserService, private _route: Router) {
     _user.profile().subscribe((res: any) => {
       if (res.apiStatus) {
@@ -24,7 +25,11 @@ export class AppComponent{
       else this.arrowStatus = false;
     };
   }
-
+  ngAfterViewInit() {
+   setTimeout(() => {
+      this.screenLoading = false;
+   }, 1000);
+  }
   goUpHandler = (e: any) => {
     e.preventDefault();
     let p = window.pageYOffset;
